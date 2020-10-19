598a97713d720cd30829abcadbb84ef1e6cb008a
harper-joo-lee <lamantciel@naver.com>
2020-10-19 23:23:48 +0900

    update - 10/19

--- src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
+++ src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -18,7 +18,5 @@ public class NotEnoughStockException extends RuntimeException{
         super(cause);
     }
 
-    protected NotEnoughStockException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
+
 }
---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+}
